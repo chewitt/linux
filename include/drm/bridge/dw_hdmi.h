d2069ccfe69c137b7057481b8a1bb836a3ef9901
Jonas Karlman <jonas@kwiboo.se>
2026-05-18 18:01:57 +0000

    FROMLIST(v7): drm: bridge: dw_hdmi: Remove the empty dw_hdmi_setup_rx_sense()
    The dw_hdmi_setup_rx_sense() helper is empty and no longer needed after
    recent RXSENSE and HPD rework, remove it.
    
    Tested-by: Diederik de Haas <diederik@cknow-tech.com>  # Rock64, RockPro64, Quartz64-B
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -186,8 +186,6 @@ struct dw_hdmi *dw_hdmi_bind(struct platform_device *pdev,
 
 void dw_hdmi_resume(struct dw_hdmi *hdmi);
 
-void dw_hdmi_setup_rx_sense(struct dw_hdmi *hdmi, bool hpd, bool rx_sense);
-
 int dw_hdmi_set_plugged_cb(struct dw_hdmi *hdmi, hdmi_codec_plugged_cb fn,
 			   struct device *codec_dev);
 void dw_hdmi_set_sample_non_pcm(struct dw_hdmi *hdmi, unsigned int non_pcm);
