ccb01d8b714f33ed9112198c4fa0989de603a8e8
Jonas Karlman <jonas@kwiboo.se>
2026-05-18 18:01:57 +0000

    FROMLIST(v7): drm: bridge: dw_hdmi: Remove the empty dw_hdmi_setup_rx_sense()
    The dw_hdmi_setup_rx_sense() helper is empty and no longer needed after
    recent RXSENSE and HPD rework, remove it.
    
    Tested-by: Diederik de Haas <diederik@cknow-tech.com>  # Rock64, RockPro64, Quartz64-B
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -3007,11 +3007,6 @@ static irqreturn_t dw_hdmi_hardirq(int irq, void *dev_id)
 	return ret;
 }
 
-void dw_hdmi_setup_rx_sense(struct dw_hdmi *hdmi, bool hpd, bool rx_sense)
-{
-}
-EXPORT_SYMBOL_GPL(dw_hdmi_setup_rx_sense);
-
 static irqreturn_t dw_hdmi_irq(int irq, void *dev_id)
 {
 	struct dw_hdmi *hdmi = dev_id;
