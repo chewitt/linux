9578aea1d17648dbd9b76ddb9b8167a41aae3bda
Detlev Casanova <detlev.casanova@collabora.com>
2026-08-10 11:22:12 -0400

    FROMLIST(v2): media: rkvdec: Remove unused need_reset
    A left-over from the iommu restore mecanism was forgotten.
    As need_reset is never set to true, the if has no use.
    
    The actual restore function is called above it vase the IRQ isn't in a
    success status.
    
    Signed-off-by: Detlev Casanova <detlev.casanova@collabora.com>

--- drivers/media/platform/rockchip/rkvdec/rkvdec.c
+++ drivers/media/platform/rockchip/rkvdec/rkvdec.c
@@ -1668,7 +1668,6 @@ static irqreturn_t vdpu381_irq_handler(struct rkvdec_ctx *ctx)
 {
 	struct rkvdec_dev *rkvdec = ctx->dev;
 	enum vb2_buffer_state state;
-	bool need_reset = 0;
 	u32 status;
 
 	status = readl(rkvdec->regs + VDPU381_REG_STA_INT);
@@ -1684,9 +1683,6 @@ static irqreturn_t vdpu381_irq_handler(struct rkvdec_ctx *ctx)
 			rkvdec_iommu_restore(rkvdec);
 	}
 
-	if (need_reset)
-		rkvdec_iommu_restore(rkvdec);
-
 	if (cancel_delayed_work(&rkvdec->watchdog_work))
 		rkvdec_job_finish(ctx, state);
 
@@ -1697,7 +1693,6 @@ static irqreturn_t vdpu383_irq_handler(struct rkvdec_ctx *ctx)
 {
 	struct rkvdec_dev *rkvdec = ctx->dev;
 	enum vb2_buffer_state state;
-	bool need_reset = 0;
 	u32 status;
 
 	status = readl(rkvdec->link + VDPU383_LINK_STA_INT);
@@ -1712,9 +1707,6 @@ static irqreturn_t vdpu383_irq_handler(struct rkvdec_ctx *ctx)
 		state = VB2_BUF_STATE_ERROR;
 		rkvdec_iommu_restore(rkvdec);
 	}
-
-	if (need_reset)
-		rkvdec_iommu_restore(rkvdec);
 
 	if (cancel_delayed_work(&rkvdec->watchdog_work))
 		rkvdec_job_finish(ctx, state);
